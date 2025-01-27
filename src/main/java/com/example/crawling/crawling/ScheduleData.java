b87e68081af339a0da523793404fabc7fbd915d9
park <acd2283@gmail.com>
2025-01-27 19:37:41 +0900

    refactor ì레코드 코드 리팩토링

---
+++ src/main/java/com/example/crawling/crawling/ScheduleData.java
@@ -0,0 +1,11 @@
+package com.example.crawling.crawling;
+
+public record ScheduleData(
+        String date,
+        String startTime,
+        String matchStatus,
+        String team1,
+        String team2,
+        String teamImg1,
+        String teamImg2
+) {}
